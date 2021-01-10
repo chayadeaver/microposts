422ef93644ed4fb594d04d4ee6ddd643ac800f19
chayadeaver <chayadeaver@gmail.com>
2021-01-10 14:03:59 -0500

    changeFormState fn to change btn and add cancel

--- src/ui.js
+++ src/ui.js
@@ -77,6 +77,26 @@ class UI {
     this.titleInput.value = data.title;
     this.bodyInput.value = data.body;
     this.idInput.value = data.id
+
+    this.changeFormState('edit');
+  }
+
+  // Change form state
+  changeFormState(type){
+    if(type === 'edit'){
+      this.postSubmit.textContent = 'Update Post';
+      this.postSubmit.className = 'post-submit btn btn-warning btn-block';
+
+      // Create cancel button
+      const button = document.createElement('button');
+      button.className = 'post-cancel btn btn-light btn-block';
+      button.appendChild(document.createTextNode('Cancel Edit'));
+
+      // Get parent
+      
+    } else {
+
+    }
   }
 
 }
